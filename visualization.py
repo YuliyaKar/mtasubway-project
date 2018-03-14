e23f70ced6b379ef43e44fab21ded7eafddc5d3e
YuliyaK <y.karanouskaya@gmail.com>
2018-03-14 03:14:47 +0100

    Plot unit entries vs rain

--- visualization.py
+++ visualization.py
@@ -3,7 +3,7 @@ import seaborn as sns
 
 def entries_by_hour(data):
     """
-        Plot bar plot showing mean number of hourly entries as
+        Plot a bar plot showing mean number of hourly entries as
         a function of hour.
     """
 
@@ -12,6 +12,22 @@ def entries_by_hour(data):
     plt.xlabel('Hour')
     plt.ylabel('Mean number of entries')
     plt.title('Hourly Number of Entries')
+
+    plt.show()
+
+    return fig
+
+def unit_entries_rain(data):
+    """
+        Plot a bar plot showing mean number of entries for each turnstile unit
+        indicating rainy (or not) weather.
+    """
+
+    fig = plt.figure()
+    sns.barplot(x='UNIT', y='ENTRIESn_hourly', hue='rain', data=data)
+    plt.xlabel('Turnstile Unit')
+    plt.ylabel('Mean number of entries')
+    plt.title('Entries at Units')
 
     plt.show()
 
