55102a46b638a00bc1df01d4d7f5afdfe9b38764
YuliyaK <y.karanouskaya@gmail.com>
2018-03-15 03:03:30 +0100

    Add weather count reducer

--- mapreduce_exercises.py
+++ mapreduce_exercises.py
@@ -1,5 +1,6 @@
 import string
 import csv
+import pandas as pd
 
 def mapper(fin_path, fout_path):
     """
@@ -28,6 +29,48 @@ def mapper(fin_path, fout_path):
             continue
 
         writer_out.writerow([format_key(float(line[14]), float(line[15]))] + [line[6]])
+
+    fin.close()
+    fout.close()
+
+def reducer(fin_path, fout_path):
+    """
+        Given the output of the mapper, the reducer writes one
+        row per weather type, along with the average value of
+        ENTRIESn_hourly for that weather type.
+        The input is assumed to be sorted by weather type.
+    """
+
+    fin = open(fin_path, 'r')
+    fout = open(fout_path, 'w')
+
+    reader_in = csv.reader(fin, delimiter=",")
+    writer_out = csv.writer(fout, delimiter=",")
+
+    riders = 0      # The number of total tiders for this key
+    num_hours = 0   # The number of hours with this key
+    old_key = None
+
+    for line in reader_in:
+
+        # Omit irregular lines
+        #if len(line) != 2:
+        #    continue
+
+        this_key, count_hours = line[0], float(line[1])
+
+        if old_key and old_key != this_key:
+
+            writer_out.writerow([old_key] + [num_hours / riders])
+            riders = 0
+            num_hours = 0
+
+        old_key = this_key
+        num_hours += count_hours
+        riders += 1.
+
+    if old_key:
+        writer_out.writerow([old_key] + [num_hours / riders])
 
     fin.close()
     fout.close()
